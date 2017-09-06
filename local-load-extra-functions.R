1ea993b19e6024a5e0355c2f9460016b6b30d34d
Chris Fields <cjfields@illinois.edu>
2017-09-06 09:09:32 -0500

    add local hack script for sourcing files from a local repo (development)

---
+++ local-load-extra-functions.R
@@ -0,0 +1,26 @@
+## Load packages
+library(ggplot2)
+library(phyloseq)
+library(reshape2)
+library(ape)
+library(gridExtra)
+
+scripts <- c("graphical_methods.R",
+             "tree_methods.R",
+             "plot_merged_trees.R",
+             "specificity_methods.R",
+             "ternary_plot.R",
+             "richness.R",
+             "edgePCA.R",
+             "copy_number_correction.R",
+             "import_frogs.R",
+             "prevalence.R", 
+             "compute_niche.R")
+
+script.dir <- dirname(sys.frame(1)$ofile)
+urls <- file.path(script.dir, scripts)
+
+for (url in urls) {
+  source(url)
+}
+
