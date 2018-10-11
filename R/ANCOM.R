fc413752e8c3c2884a4893088bc4f2713cfba91f
Chris Fields <cjfields@illinois.edu>
2018-10-11 16:10:27 -0500

    comment out dynamic code examples

--- R/ANCOM.R
+++ R/ANCOM.R
@@ -197,115 +197,115 @@ ANCOM.main = function(OTUdat,Vardat,
 
 
 ## Example
-ys_data=read.delim("~/Dropbox/Work at NIEHS/sid_reference_data/ys_genera.txt",header=T)
-
-otu_test=ys_data[,c(1,5:665)]
-map_test=ys_data[,c(1:4)]
-  
-
-comparison_test=ANCOM.main(OTUdat=otu_test,
-                           Vardat=map_test[which(map_test$SEX!="unknown"&map_test$COUNTRY!="GAZ:Venezuela"),],
-                           adjusted=TRUE,
-                           repeated=F,
-                           main.var="COUNTRY",
-                           adj.formula="SEX+AGE",
-                           repeat.var=NULL,
-                           multcorr=2,
-                           sig=0.05,
-                           prev.cut=0.90)
-
-comparison_test$W.taxa
 
 
-
+# ys_data=read.delim("~/Dropbox/Work at NIEHS/sid_reference_data/ys_genera.txt",header=T)
+# 
+# otu_test=ys_data[,c(1,5:665)]
+# map_test=ys_data[,c(1:4)]
+#   
+# 
+# comparison_test=ANCOM.main(OTUdat=otu_test,
+#                            Vardat=map_test[which(map_test$SEX!="unknown"&map_test$COUNTRY!="GAZ:Venezuela"),],
+#                            adjusted=TRUE,
+#                            repeated=F,
+#                            main.var="COUNTRY",
+#                            adj.formula="SEX+AGE",
+#                            repeat.var=NULL,
+#                            multcorr=2,
+#                            sig=0.05,
+#                            prev.cut=0.90)
+# 
+# comparison_test$W.taxa
 
 ####Longitudinal test
-OTU_ANCOM_Test <- read.delim("~/Dropbox/Work at NIEHS/OTU_Ancom_Test_Counts.txt")
 
-Mapping_File_ANCOM_Test <- read.delim("~/Dropbox/Work at NIEHS/OTU_Ancom_Test_Mapping.txt")
+# OTU_ANCOM_Test <- read.delim("~/Dropbox/Work at NIEHS/OTU_Ancom_Test_Counts.txt")
+# 
+# Mapping_File_ANCOM_Test <- read.delim("~/Dropbox/Work at NIEHS/OTU_Ancom_Test_Mapping.txt")
 
 #data_test=merge(OTU_ANCOM_Test,Mapping_File_ANCOM_Test,by="Sample.ID")
 
-longitudinal_comparison_test=ANCOM.main(OTUdat=OTU_ANCOM_Test,
-                                        Vardat=Mapping_File_ANCOM_Test[Mapping_File_ANCOM_Test$group_info%in%c("2FL","3SL","pHMO"),],
-                                        adjusted=F,
-                                        repeated=T,
-                                        main.var="group_info",
-                                        adj.formula=NULL,
-                                        repeat.var="day_of_treatment",
-                                        longitudinal=T,
-                                        random.formula="~1|host_subject_id",
-                                        multcorr=2,
-                                        sig=0.05,
-                                        prev.cut=0.90)
-
-longitudinal_comparison_test$W.taxa
-
-
-longitudinal_comparison_test=ANCOM.main(OTUdat=OTU_ANCOM_Test,
-                                        Vardat=Mapping_File_ANCOM_Test[Mapping_File_ANCOM_Test$group_info%in%c("2FL","3SL","pHMO"),],
-                                        adjusted=F,
-                                        repeated=T,
-                                        main.var="group_info",
-                                        adj.formula=NULL,
-                                        repeat.var="day_of_treatment",
-                                        longitudinal=T,
-                                        random.formula="~1|host_subject_id",
-                                        multcorr=2,
-                                        sig=0.05,
-                                        prev.cut=0.90)
-
-longitudinal_comparison_test$W.taxa
-
-
-
-
-longitudinal_comparison_test_HC_RA=ANCOM.main(OTUdat=otu_test,
-                                        Vardat=map_test[which(map_test$Group%in%c("HC","RA")),],
-                                        adjusted=F,
-                                        repeated=T,
-                                        main.var="Group",
-                                        adj.formula=NULL,
-                                        repeat.var="Visitorder",
-                                        longitudinal=T,
-                                        random.formula="~1|individual",
-                                        multcorr=2,
-                                        sig=0.05,
-                                        prev.cut=0.50)
-
-longitudinal_comparison_test_HC_RA$W.taxa
-
-
-
-longitudinal_comparison_test_HC_RR=ANCOM.main(OTUdat=otu_test,
-                                              Vardat=map_test[which(map_test$Group%in%c("HC","RR")),],
-                                              adjusted=F,
-                                              repeated=T,
-                                              main.var="Group",
-                                              adj.formula=NULL,
-                                              repeat.var="Visitorder",
-                                              longitudinal=T,
-                                              random.formula="~1|individual",
-                                              multcorr=2,
-                                              sig=0.05,
-                                              prev.cut=0.50)
-
-longitudinal_comparison_test_HC_RR$W.taxa
-
-longitudinal_comparison_test_RA_RR=ANCOM.main(OTUdat=otu_test,
-                                              Vardat=map_test[which(map_test$Group%in%c("RA","RR")),],
-                                              adjusted=F,
-                                              repeated=T,
-                                              main.var="Group",
-                                              adj.formula=NULL,
-                                              repeat.var="Visitorder",
-                                              longitudinal=T,
-                                              random.formula="~1|individual",
-                                              multcorr=2,
-                                              sig=0.05,
-                                              prev.cut=0.50)
-
-longitudinal_comparison_test_RA_RR$W.taxa
+# longitudinal_comparison_test=ANCOM.main(OTUdat=OTU_ANCOM_Test,
+#                                         Vardat=Mapping_File_ANCOM_Test[Mapping_File_ANCOM_Test$group_info%in%c("2FL","3SL","pHMO"),],
+#                                         adjusted=F,
+#                                         repeated=T,
+#                                         main.var="group_info",
+#                                         adj.formula=NULL,
+#                                         repeat.var="day_of_treatment",
+#                                         longitudinal=T,
+#                                         random.formula="~1|host_subject_id",
+#                                         multcorr=2,
+#                                         sig=0.05,
+#                                         prev.cut=0.90)
+# 
+# longitudinal_comparison_test$W.taxa
+# 
+# 
+# longitudinal_comparison_test=ANCOM.main(OTUdat=OTU_ANCOM_Test,
+#                                         Vardat=Mapping_File_ANCOM_Test[Mapping_File_ANCOM_Test$group_info%in%c("2FL","3SL","pHMO"),],
+#                                         adjusted=F,
+#                                         repeated=T,
+#                                         main.var="group_info",
+#                                         adj.formula=NULL,
+#                                         repeat.var="day_of_treatment",
+#                                         longitudinal=T,
+#                                         random.formula="~1|host_subject_id",
+#                                         multcorr=2,
+#                                         sig=0.05,
+#                                         prev.cut=0.90)
+# 
+# longitudinal_comparison_test$W.taxa
+# 
+# 
+# 
+# 
+# longitudinal_comparison_test_HC_RA=ANCOM.main(OTUdat=otu_test,
+#                                         Vardat=map_test[which(map_test$Group%in%c("HC","RA")),],
+#                                         adjusted=F,
+#                                         repeated=T,
+#                                         main.var="Group",
+#                                         adj.formula=NULL,
+#                                         repeat.var="Visitorder",
+#                                         longitudinal=T,
+#                                         random.formula="~1|individual",
+#                                         multcorr=2,
+#                                         sig=0.05,
+#                                         prev.cut=0.50)
+# 
+# longitudinal_comparison_test_HC_RA$W.taxa
+# 
+# 
+# 
+# longitudinal_comparison_test_HC_RR=ANCOM.main(OTUdat=otu_test,
+#                                               Vardat=map_test[which(map_test$Group%in%c("HC","RR")),],
+#                                               adjusted=F,
+#                                               repeated=T,
+#                                               main.var="Group",
+#                                               adj.formula=NULL,
+#                                               repeat.var="Visitorder",
+#                                               longitudinal=T,
+#                                               random.formula="~1|individual",
+#                                               multcorr=2,
+#                                               sig=0.05,
+#                                               prev.cut=0.50)
+# 
+# longitudinal_comparison_test_HC_RR$W.taxa
+# 
+# longitudinal_comparison_test_RA_RR=ANCOM.main(OTUdat=otu_test,
+#                                               Vardat=map_test[which(map_test$Group%in%c("RA","RR")),],
+#                                               adjusted=F,
+#                                               repeated=T,
+#                                               main.var="Group",
+#                                               adj.formula=NULL,
+#                                               repeat.var="Visitorder",
+#                                               longitudinal=T,
+#                                               random.formula="~1|individual",
+#                                               multcorr=2,
+#                                               sig=0.05,
+#                                               prev.cut=0.50)
+# 
+# longitudinal_comparison_test_RA_RR$W.taxa
 
 
 # plots_for_ancom=function(otu_list,otu_data,metadata,var){
